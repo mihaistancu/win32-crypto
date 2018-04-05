48b8164bd13f9ad6c64825335b763ed51f71b656
mihai <mihai@EESSIDEV04>
2018-04-05 14:14:32 +0200

    Verify revocation using OCSP

--- Certificates/Certificates/Source.cpp
+++ Certificates/Certificates/Source.cpp
@@ -12,6 +12,20 @@ void main()
 		L"leaf",
 		NULL);
 
+	PVOID dwCertPtr[] = { (PVOID)pCertContext };
+
+	CERT_REVOCATION_STATUS revocationStatus;
+	revocationStatus.cbSize = sizeof(CERT_REVOCATION_STATUS);
+
+	BOOL verificationResult = CertVerifyRevocation(
+		X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
+		CERT_CONTEXT_REVOCATION_TYPE,
+		1,
+		dwCertPtr,
+		CERT_VERIFY_REV_SERVER_OCSP_FLAG,
+		NULL,
+		&revocationStatus);
+
 	CertFreeCertificateContext(pCertContext);
 
 	CertCloseStore(hCertStore, 0);
