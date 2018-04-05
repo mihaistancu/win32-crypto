c5018952c867bb688e3bee09b2cc0d756db40e24
mihai <mihai@EESSIDEV04>
2018-04-05 13:46:43 +0200

    Get certificate from store

--- Certificates/Certificates/Source.cpp
+++ Certificates/Certificates/Source.cpp
@@ -4,5 +4,15 @@ void main()
 {
 	HCERTSTORE hCertStore = CertOpenSystemStore(NULL, L"My");
 
+	PCCERT_CONTEXT pCertContext = CertFindCertificateInStore(
+		hCertStore,
+		PKCS_7_ASN_ENCODING | X509_ASN_ENCODING,
+		0,
+		CERT_FIND_SUBJECT_STR,
+		L"leaf",
+		NULL);
+
+	CertFreeCertificateContext(pCertContext);
+
 	CertCloseStore(hCertStore, 0);
 }
