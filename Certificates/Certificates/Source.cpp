a311f706ab506572a0455ab5a86155a3735b7ca4
mihai <mihai@EESSIDEV04>
2018-04-05 14:19:55 +0200

    Refactoring

--- Certificates/Certificates/Source.cpp
+++ Certificates/Certificates/Source.cpp
@@ -1,23 +1,24 @@
 #include <windows.h>
 
-void main()
+PCCERT_CONTEXT GetCertificate(HCERTSTORE hCertStore)
 {
-	HCERTSTORE hCertStore = CertOpenSystemStore(NULL, L"My");
-
-	PCCERT_CONTEXT pCertContext = CertFindCertificateInStore(
+	return CertFindCertificateInStore(
 		hCertStore,
 		PKCS_7_ASN_ENCODING | X509_ASN_ENCODING,
 		0,
 		CERT_FIND_SUBJECT_STR,
 		L"leaf",
 		NULL);
+}
 
+bool CheckOcsp(PCCERT_CONTEXT pCertContext)
+{
 	PVOID dwCertPtr[] = { (PVOID)pCertContext };
 
 	CERT_REVOCATION_STATUS revocationStatus;
 	revocationStatus.cbSize = sizeof(CERT_REVOCATION_STATUS);
 
-	BOOL verificationResult = CertVerifyRevocation(
+	return CertVerifyRevocation(
 		X509_ASN_ENCODING | PKCS_7_ASN_ENCODING,
 		CERT_CONTEXT_REVOCATION_TYPE,
 		1,
@@ -25,6 +26,15 @@ void main()
 		CERT_VERIFY_REV_SERVER_OCSP_FLAG,
 		NULL,
 		&revocationStatus);
+}
+
+void main()
+{
+	HCERTSTORE hCertStore = CertOpenSystemStore(NULL, L"My");
+
+	PCCERT_CONTEXT pCertContext = GetCertificate(hCertStore);
+
+	BOOL isOcspValid = CheckOcsp(pCertContext);
 
 	CertFreeCertificateContext(pCertContext);
 
