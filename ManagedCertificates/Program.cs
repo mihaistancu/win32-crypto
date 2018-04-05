067a67954e34613ddf08e15fc6e1256ac38d0673
mihai <mihai@EESSIDEV04>
2018-04-05 19:53:52 +0200

    Get certificate from store

--- ManagedCertificates/Program.cs
+++ ManagedCertificates/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace ManagedCertificates
 {
     class Program
     {
+        static IntPtr GetCertificate(IntPtr hCertStore)
+        {
+            int dwEncoding = Win32.PKCS_7_ASN_ENCODING | Win32.X509_ASN_ENCODING;
+            int dwFindFlags = 0;
+            int dwFindType = Win32.CERT_FIND_SUBJECT_STR;
+            IntPtr pvFindParam = Marshal.StringToHGlobalUni("leaf");
+            IntPtr pPrevCertContext = IntPtr.Zero;
+
+            return Win32.CertFindCertificateInStore(hCertStore, dwEncoding, dwFindFlags, dwFindType, pvFindParam, pPrevCertContext);
+        }
+
         static void Main(string[] args)
         {
             IntPtr hCertStore = Win32.CertOpenSystemStore(IntPtr.Zero, "My");
+
+            IntPtr hCertContext = GetCertificate(hCertStore);
         }
     }
 }
--- ManagedCertificates/Win32.cs
+++ ManagedCertificates/Win32.cs
@@ -5,7 +5,14 @@ namespace ManagedCertificates
 {
     class Win32
     {
+        public const int CERT_FIND_SUBJECT_STR = 0x00080007;
+        public const int X509_ASN_ENCODING = 0x00000001;
+        public const int PKCS_7_ASN_ENCODING = 0x00010000;
+
         [DllImport("crypt32.dll")]
         public static extern IntPtr CertOpenSystemStore(IntPtr hprov, string szSubsystemProtocol);
+
+        [DllImport("crypt32.dll")]
+        public static extern IntPtr CertFindCertificateInStore(IntPtr hCertStore, int dwEncoding, int dwFindFlags, int dwFindType, IntPtr pvFindParam, IntPtr pPrevCertContext);
     }
 }
