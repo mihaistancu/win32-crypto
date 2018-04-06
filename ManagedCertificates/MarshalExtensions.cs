eafca0c9bd88c320113a6f875460b33caeb1f1d0
mihaistancu <stancu_meehigh@yahoo.com>
2018-04-06 15:34:26 +0300

    Add comment with explanation regarding structs with variable sized arrays

--- ManagedCertificates/MarshalExtensions.cs
+++ ManagedCertificates/MarshalExtensions.cs
@@ -5,6 +5,7 @@ namespace ManagedCertificates
 {
     public static class MarshalExtensions
     {
+        // This is needed because structs with variable-sized arrays cannot be marshaled directly
         public static string[] PtrToStringArray(IntPtr pointer, int arrayLength)
         {
             IntPtr[] outPointers = new IntPtr[arrayLength];
