85dbbc764659d6d3e15869c72fea03b58736199c
mihaistancu <stancu_meehigh@yahoo.com>
2018-04-09 18:32:12 +0300

    Map DWORD to uint instead of int, according to https://msdn.microsoft.com/en-us/library/cc230318.aspx

--- ManagedCertificates/MarshalExtensions.cs
+++ ManagedCertificates/MarshalExtensions.cs
@@ -6,10 +6,10 @@ namespace ManagedCertificates
     public static class MarshalExtensions
     {
         // This is needed because structs with variable-sized arrays cannot be marshaled directly
-        public static string[] PtrToStringArray(IntPtr pointer, int arrayLength)
+        public static string[] PtrToStringArray(IntPtr pointer, uint arrayLength)
         {
             IntPtr[] outPointers = new IntPtr[arrayLength];
-            Marshal.Copy(pointer, outPointers, 0, arrayLength);
+            Marshal.Copy(pointer, outPointers, 0, (int)arrayLength);
 
             var result = new string[arrayLength];
             for (int i = 0; i < arrayLength; i++)
--- ManagedCertificates/Win32.cs
+++ ManagedCertificates/Win32.cs
@@ -8,39 +8,39 @@ namespace ManagedCertificates
         [StructLayout(LayoutKind.Sequential)]
         public class CRYPT_URL_ARRAY
         {
-            public int cUrl;
+            public uint cUrl;
             public IntPtr rgwszUrl;           
         }
         
         [StructLayout(LayoutKind.Sequential)]
         public class CERT_REVOCATION_STATUS
         {
-            public int cbSize;
-            public int dwIndex;
-            public int dwError;
-            public int dwReason;
+            public uint cbSize;
+            public uint dwIndex;
+            public uint dwError;
+            public uint dwReason;
             public bool fHasFreshnessTime;
-            public int dwFreshnessTime;
+            public uint dwFreshnessTime;
         }
 
         public static IntPtr URL_OID_CERTIFICATE_CRL_DIST_POINT = new IntPtr(2);
         public static IntPtr CONTEXT_OID_CRL = new IntPtr(2);
-        public const int CERT_VERIFY_REV_SERVER_OCSP_FLAG = 0x00000008;
-        public const int CERT_CONTEXT_REVOCATION_TYPE = 1;
-        public const int X509_ASN_ENCODING = 0x00000001;
-        public const int PKCS_7_ASN_ENCODING = 0x00010000;
+        public const uint CERT_VERIFY_REV_SERVER_OCSP_FLAG = 0x00000008;
+        public const uint CERT_CONTEXT_REVOCATION_TYPE = 1;
+        public const uint X509_ASN_ENCODING = 0x00000001;
+        public const uint PKCS_7_ASN_ENCODING = 0x00010000;
         
         [DllImport("crypt32.dll")]
-        public static extern bool CertVerifyRevocation(int dwEncoding, int dwRevType, int cContext, IntPtr[] rgpvContext, int dwFlags, IntPtr pRevPara, CERT_REVOCATION_STATUS revocationStatus);
+        public static extern bool CertVerifyRevocation(uint dwEncoding, uint dwRevType, uint cContext, IntPtr[] rgpvContext, uint dwFlags, IntPtr pRevPara, CERT_REVOCATION_STATUS revocationStatus);
         
         [DllImport("cryptnet.dll")]
-        public static extern bool CryptGetObjectUrl(IntPtr pszUrlOid, IntPtr pvPara, int dwFlags, IntPtr pUrlArray, ref int size, IntPtr pUrlInfo, IntPtr pcbUrlInfo, IntPtr pvReserved);
+        public static extern bool CryptGetObjectUrl(IntPtr pszUrlOid, IntPtr pvPara, uint dwFlags, IntPtr pUrlArray, ref uint pcbUrlArray, IntPtr pUrlInfo, ref uint pcbUrlInfo, IntPtr pvReserved);
 
         [DllImport("cryptnet.dll")]
-        public static extern bool CryptRetrieveObjectByUrl(string pszUrl, IntPtr pszObjectOid, int dwRetrievalFlags, int dwTimeout, ref IntPtr ppvObject, IntPtr hAsyncRetrieve, IntPtr pCredentials, IntPtr pvVerify, IntPtr pAuxInfo);
+        public static extern bool CryptRetrieveObjectByUrl(string pszUrl, IntPtr pszObjectOid, uint dwRetrievalFlags, uint dwTimeout, ref IntPtr ppvObject, IntPtr hAsyncRetrieve, IntPtr pCredentials, IntPtr pvVerify, IntPtr pAuxInfo);
 
         [DllImport("crypt32.dll")]
-        public static extern void CertFindCertificateInCRL(IntPtr pCertContext, IntPtr pCrlContext, int dwFlags, IntPtr pvReserved, ref IntPtr pCrlEntry);
+        public static extern void CertFindCertificateInCRL(IntPtr pCertContext, IntPtr pCrlContext, uint dwFlags, IntPtr pvReserved, ref IntPtr pCrlEntry);
 
         [DllImport("crypt32.dll")]
         public static extern void CertFreeCRLContext(IntPtr pCrlContext);
