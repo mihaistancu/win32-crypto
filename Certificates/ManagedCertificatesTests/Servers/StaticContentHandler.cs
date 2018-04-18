48d6e206ba56803988734d4a27ba757d66266e08
mihai <mihai@EESSIDEV04>
2018-04-18 14:13:20 +0200

    Added static http server

---
+++ Certificates/ManagedCertificatesTests/Servers/StaticContentHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ManagedCertificatesTests.Servers
+{
+    public class StaticContentHandler : HttpMessageHandler
+    {
+        private readonly HttpResponseMessage response;
+
+        public StaticContentHandler(HttpResponseMessage response)
+        {
+            this.response = response;
+        }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(response);
+        }
+    }
+}
---
+++ Certificates/ManagedCertificatesTests/Servers/StaticHttpServer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+
+namespace ManagedCertificatesTests.Servers
+{
+    public class StaticHttpServer
+    {
+        public static IDisposable Start(string baseAddress, Dictionary<string, HttpResponseMessage> config)
+        {
+            var server = new HttpServer();
+            foreach (var item in config)
+            {
+                server.Setup(item.Key, new StaticContentHandler(item.Value));
+            }
+            server.Start(baseAddress);
+            return server;
+        }
+    }
+}
