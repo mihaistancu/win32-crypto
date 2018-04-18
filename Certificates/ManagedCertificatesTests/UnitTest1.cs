aebdaff5eb40d63124ba5fe889ef9dcb757c5878
mihai <mihai@EESSIDEV04>
2018-04-18 14:00:30 +0200

    Added method to check revocation in unit tests

--- Certificates/ManagedCertificatesTests/UnitTest1.cs
+++ Certificates/ManagedCertificatesTests/UnitTest1.cs
@@ -1,4 +1,9 @@
-﻿using ManagedCertificatesTests.Certificates;
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
+using ManagedCertificates.Revocation.Exceptions;
+using ManagedCertificates.Win32;
+using ManagedCertificatesTests.Certificates;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace ManagedCertificatesTests
@@ -28,8 +33,25 @@ namespace ManagedCertificatesTests
                 CrlEndpoints = new [] {"http://localhost:9090/crl1", "http://localhost:9090/crl2", "http://localhost:9090/crl3" }
             };
             var leaf = leafGenerator.Generate();
+            
+        }
 
+        public static void Check(X509Certificate2 certificate)
+        {
+            uint dwEncoding = CAPI.PKCS_7_ASN_ENCODING | CAPI.X509_ASN_ENCODING;
+            uint dwRevType = CAPI.CERT_CONTEXT_REVOCATION_TYPE;
+            uint cContext = 1;
+            IntPtr[] rgpvContext = { certificate.Handle };
+            uint dwFlags = 0;
+            IntPtr pRevPara = IntPtr.Zero;
+            var revocationStatus = new CAPI.CERT_REVOCATION_STATUS(Marshal.SizeOf(typeof(CAPI.CERT_REVOCATION_STATUS)));
 
+            bool isGood = CAPI.CertVerifyRevocation(dwEncoding, dwRevType, cContext, rgpvContext, dwFlags, pRevPara, revocationStatus);
+
+            if (!isGood)
+            {
+                throw new RevocationException(revocationStatus.dwError, revocationStatus.dwReason);
+            }
         }
     }
 }
