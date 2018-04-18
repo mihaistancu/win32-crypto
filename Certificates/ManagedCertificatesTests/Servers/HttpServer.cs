8d1769a89d7517887db182b53f686eec7016ed6a
mihai <mihai@EESSIDEV04>
2018-04-18 14:11:25 +0200

    Added http server

---
+++ Certificates/ManagedCertificatesTests/Servers/HttpServer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using Owin;
+
+namespace ManagedCertificatesTests.Servers
+{
+    public class HttpServer : IDisposable
+    {
+        private readonly HttpConfiguration config;
+        private IDisposable server;
+
+        public HttpServer()
+        {
+            config = new HttpConfiguration();
+        }
+
+        public void Setup(string path, HttpMessageHandler handler)
+        {
+            config.Routes.MapHttpRoute(path, path + "/{*url}", new { url = RouteParameter.Optional }, null, handler);
+        }
+
+        public void Start(string baseAddress)
+        {
+            server = WebApp.Start(baseAddress, appBuilder => appBuilder.UseWebApi(config));
+        }
+
+        public void Dispose()
+        {
+            server?.Dispose();
+        }
+    }
+}
