fcaebe13cdb2dff83812ac708c53d964898c0409
mihai <mihai@EESSIDEV04>
2018-04-18 10:29:04 +0200

    Added new unit tests project

---
+++ Certificates/ManagedCertificatesTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ManagedCertificatesTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
